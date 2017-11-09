4e212d988855ce9d924705bf23c32d225c659ddb
Fabian Gut <fabian.gut@ethy.ch>
2017-11-09 07:25:51 +0100

    LoginComponent needs to close modal when error is returned

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { GraphqlActions } from '../../graphql/graphql.actions';
 import { NgRedux, select, WithSubStore } from '@angular-redux/store';
 import { authReducer } from '../auth.reducer';
 import { Observable } from 'rxjs/Observable';
 import { Router } from '@angular/router';
 import { IAppState } from '../../store/root.model';
-import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbModalOptions, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { LoadingModalComponent } from '../../layout/loading-modal/loading-modal.component';
+import { IAuthState } from '../auth.model';
 
 @WithSubStore({
   basePathMethodName: 'getBasePath',
@@ -18,12 +19,13 @@ import { LoadingModalComponent } from '../../layout/loading-modal/loading-modal.
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnDestroy {
-  @select() private readonly error$: Observable<string>;
+  @select() private error$: Observable<string>;
 
   private subscription;
+
   private username;
   private password;
-  private loadingModalRef;
+  private loadingModalRef: NgbModalRef;
 
   constructor(
     private graphqlActions: GraphqlActions,
@@ -31,25 +33,26 @@ export class LoginComponent implements OnDestroy {
     private ngRedux: NgRedux<IAppState>,
     private router: Router
   ) {
-    this.subscription = this.ngRedux.select<string>(['auth', 'token'])
-      .subscribe(newToken => {
-        if (newToken !== null) {
-          localStorage.setItem('token', newToken);
+    this.subscription = this.ngRedux.select<IAuthState>(['auth'])
+      .subscribe(newAuthState => {
+        if (newAuthState.token !== null) {
+          localStorage.setItem('token', newAuthState.token);
           this.router.navigate(['']);
-          if (this.loadingModalRef) {
-            this.loadingModalRef.close();
-            this.loadingModalRef = null;
-          }
+          this.closeLoading();
+        } else if (newAuthState.error !== null) {
+          this.closeLoading();
         }
       });
   }
 
   login() {
-    this.openLoading();
+    setTimeout(() => {
+      this.openLoading();
+    });
     this.graphqlActions.login(this.username, this.password);
   }
 
-  openLoading() {
+  private openLoading() {
     const options: NgbModalOptions = {
       backdrop: 'static',
       keyboard: false,
@@ -58,6 +61,14 @@ export class LoginComponent implements OnDestroy {
     };
 
     this.loadingModalRef = this.modalService.open(LoadingModalComponent, options);
+  }
+
+  private closeLoading() {
+    if (this.loadingModalRef) {
+      this.loadingModalRef.close();
+      this.loadingModalRef.dismiss();
+      this.loadingModalRef = null;
+    }
   }
 
   getBasePath = () => 'auth';
